861befb777368544be45104b3ab0d7555582a104
VitorAraujoOliveira <vitor.araujo934@hotmail.com>
2018-11-27 21:00:09 -0200

    Add files via upload

---
+++ Banco_de_dados/inserir_eventos.sql
@@ -0,0 +1,15 @@
+﻿select * from evento;
+
+
+insert into evento
+values
+(1,'11/11/2011','11/11/2011','Evento Teste 1','UNI-FACEF',3,5,true),
+(2,'11/11/2012','11/11/2011','Evento Teste 2','UNI-FACEF',3,5,true),
+(33,'11/09/2013','11/11/2011','Evento Teste 3','UNI-FACEF',3,5,true),
+(44,'17/01/2014','11/11/2011','Evento Teste 4','UNI-FACEF',3,5,true),
+(10,'16/02/2015','11/11/2011','Evento Teste 5','UNI-FACEF',3,5,true),
+(5,'16/05/2016','11/11/2011','Evento Teste 6','UNI-FACEF',3,5,true),
+(6,'15/07/2017','11/11/2011','Evento Teste 7','UNI-FACEF',3,5,true),
+(7,'14/02/2018','11/11/2011','Evento Teste 8','UNI-FACEF',3,5,true),
+(8,'13/12/2019','11/11/2011','Evento Teste 9','UNI-FACEF',3,5,true),
+(9,'12/01/2020','11/11/2011','Evento Teste 10','UNI-FACEF',3,5,true);
